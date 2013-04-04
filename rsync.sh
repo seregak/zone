afb5a3b0b276b13e23fe78ad40253b850bbdc5d6
Serega <serega_klimkin@yahoo.com>
2013-04-04 15:47:31 +0300

    change

--- rsync.sh
+++ rsync.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-rsync -r -a -v --delete /var/www/clipzone/clip free:/home/sergey.klimkin/files/clip
-rsync -r -a -v --delete /var/www/clipzone/drive free:/home/sergey.klimkin/files/drive
-rsync -r -a -v --delete /var/www/clipzone/lessons free:/home/sergey.klimkin/files/lessons
-rsync -r -a -v --delete	/var/www/clipzone/caver free:/home/sergey.klimkin/files/caver
-rsync -r -a -v --delete	/var/www/clipzone/mp3 free:/home/sergey.klimkin/files/mp3
+rsync -r -a -v --delete /var/www/clipzone/clip free:/home/sergey.klimkin/files/
+rsync -r -a -v --delete /var/www/clipzone/drive free:/home/sergey.klimkin/files/
+rsync -r -a -v --delete /var/www/clipzone/lessons free:/home/sergey.klimkin/files/
+rsync -r -a -v --delete	/var/www/clipzone/caver free:/home/sergey.klimkin/files/
+rsync -r -a -v --delete	/var/www/clipzone/mp3 free:/home/sergey.klimkin/files/
 rsync -v /home/serega/clip.sql.gz free:/home/sergey.klimkin/files
 rsync -v /home/serega/clipzone.tgz free:/home/sergey.klimkin/files
+rm /home/serega/clip.sql.gz
+rm /home/serega/clipzone.tgz
