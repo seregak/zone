8a66b1c8505e87d071e3a5b4781e481d58010106
Serega <serega_klimkin@yahoo.com>
2013-04-16 17:12:14 +0300

    new slideshow

--- slideshow.sh
+++ slideshow.sh
@@ -1,8 +1,9 @@
 #/bin/bash
 cd /home/serega/clipzone/slideshow/
-for i in *;
+LIST=`ls | awk '{print substr($0,1,length($0)-4)}'`
+for i in $LIST;
 do
 ffmpeg -i "$i.mp4" -r 1 -ss 00:00:30.000 -vframes 1 -t 1 "$i.jpg"
-convert "$i.jpg"    -resize 250x141\!  "$i.jpg"
+convert "$i.jpg"    -resize 750x423\!  "$i.jpg"
 done
 
