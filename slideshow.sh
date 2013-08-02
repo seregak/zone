c44b10314cfedc43e260ea13d7861a52a59784cd
root <root@clipzone-aws.net>
2013-08-02 09:58:58 +0000

    slideshow modified

--- slideshow.sh
+++ slideshow.sh
@@ -7,3 +7,6 @@ ffmpeg -i "$i.mp4" -r 1 -ss 00:00:30.000 -vframes 1 -t 1 "$i.jpg"
 convert "$i.jpg"    -resize 750x423\!  "$i.jpg"
 done
 
+mv * .jpg /var/www/clipzone/images/yootheme/demo/gallery/
+rm -r /home/serega/clipzone/slideshow/*
+
