1ed1e5b02007fee87699a80d93fae844a354f20b
root <root@clipzone.net>
2013-11-24 17:08:11 +0000

    added strikead clone script

--- uncoded/rap.sh
+++ uncoded/rap.sh
@@ -3,7 +3,7 @@ cd /home/serega/uncoded/rap/
 
 for i in *
 do
-        ffmpeg -i "$i" -vcodec libx264 -movflags faststart -vprofile high -preset slower -b:v 850k -vf scale=1024:576 -threads 1 -acodec libmp3lame -b:a 192k /home/serega/files/clips/rap/"$i"
+        ffmpeg -i "$i" -vcodec libx264 -movflags faststart -vprofile high -preset slower -b:v 850k -vf scale=1024:576 -threads 0 -acodec libmp3lame -b:a 192k /home/serega/files/clips/rap/"$i"
 	/project/scripts/zone/main.sh
 done
 
